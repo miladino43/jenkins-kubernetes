0266ac7d8a05467abf3f04db5ee4b55f67931ea8
mb665616 <milad.bonakdar@broadcom.com>
2019-05-06 22:46:17 -0700

    initial commit

---
+++ create-payload.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+GIT_COMMIT=${GIT_COMMIT:-"Test Value"}
+
+
+function generate_payload {
+        cat <<eof > index.html
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kube Api</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>My Payload</h1>
+<p>My environment is DEV1</p>
+<p>The git hash is $GIT_COMMIT</p>
+<p>For online documentation and support please refer to
+<br/>
+The Readme for this repo is at 
+<a href="http://nginx.com/">nginx.com</a>.</p>
+
+<p><em>Thank you for your time.</em></p>
+</body>
+</html>
+eof
+}
+
+function main() {
+	generate_payload
+}
+
+main "$@"
